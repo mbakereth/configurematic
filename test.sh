c402d989e5986d12021e9ddf9c6387c79f3dc762
mabaker <matthew.baker@id.ethz.ch>
2024-12-12 09:34:22 +0100

    Added test file

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ $# = 0 ]; then
+    PYTHONPATH="./src/configurematic:${PYTHONPATH}" python -m unittest discover -s test -p 'test_*.py'
+else
+    PYTHONPATH="./src/configurematic:${PYTHONPATH}"  python -m unittest $1
+fi
